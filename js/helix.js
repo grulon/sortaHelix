48f5508f6fd16d4758a23516bb394ea65a3a61b6
Gavin Rulon <gavin.rulon@icloud.com>
2015-08-16 17:29:03 -0400

    Add stop and reset buttons.

--- js/helix.js
+++ js/helix.js
@@ -1,24 +1,40 @@
-function helix() {
+(function helix() {
   var context = myCanvas.getContext('2d');
+  var animate_button = document.getElementById('animate_button');
+  var stop_button = document.getElementById('stop_button');
+  var reset_button=document.getElementById('reset_button');
   var x=0, y=0, width = myCanvas.width, height = myCanvas.height;
+  var offset = 0;
+  var interval;
 
   function drawManyRectangles(){
-  	var n = 10;
-  	width = 20;
+    // debugger;  //used debugger to see what 'this' keyword returned ... fyi it was animate_button
+  	var n = document.getElementById('rect_qty').value;
+  	width = 40;
   	height = 20;
-  	var time = 0;
+
 
     var redraw = function(){
       context.clearRect(0, 0, myCanvas.width, myCanvas.height);
       for (var i=0; i < n; i++) {
-    		x = i/10 * myCanvas.width + time;
-        y = i/10 * myCanvas.height + time;
+    		x = i/10 * myCanvas.width + offset;
+        y = i/10 * myCanvas.height + offset;
     		context.fillRect(x, y, width, height);
     	 }
-       time++;
+       offset++;
     };
-    setInterval(redraw, 20);
+    stop();
+    interval= setInterval(redraw, 20);
   }
-  drawManyRectangles();
-}
-helix();
+
+  function stop() {
+    clearInterval(interval);
+  }
+
+  animate_button.onclick = drawManyRectangles;
+  stop_button.onclick = stop;
+  reset_button.onclick = function(){
+    offset = 0;
+    //document.getElementById('rect_qty').value = 20;
+  };
+})();
