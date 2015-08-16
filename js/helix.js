8855646af2e41009692e326b98b8ebe8fe095e80
Gavin Rulon <gavin.rulon@icloud.com>
2015-08-16 14:00:23 -0400

    Animate squares diagonally.

--- js/helix.js
+++ js/helix.js
@@ -3,16 +3,22 @@ function helix() {
   var x=0, y=0, width = myCanvas.width, height = myCanvas.height;
 
   function drawManyRectangles(){
-	var n = 10;
-	width = 20;
-	height = 20;
-	for (var i=0; i < n; i++) {
-		x = i/10 * myCanvas.width;
-    y = i/10 * myCanvas.height;
-		context.fillRect(x, y, width, height);
-	}
-}
-  drawManyRectangles();
+  	var n = 10;
+  	width = 20;
+  	height = 20;
+  	var time = 0;
 
+    var redraw = function(){
+      context.clearRect(0, 0, myCanvas.width, myCanvas.height);
+      for (var i=0; i < n; i++) {
+    		x = i/10 * myCanvas.width + time;
+        y = i/10 * myCanvas.height + time;
+    		context.fillRect(x, y, width, height);
+    	 }
+       time++;
+    };
+    setInterval(redraw, 20);
+  }
+  drawManyRectangles();
 }
 helix();
