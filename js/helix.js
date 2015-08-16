953e3c3f55aee620f8d358e6296ba3ab8250776b
Gavin Rulon <gavin.rulon@icloud.com>
2015-08-16 19:40:31 -0400

    Errors Resolved

--- js/helix.js
+++ js/helix.js
@@ -1,14 +1,14 @@
 (function helix() {
   var context = myCanvas.getContext('2d');
-  var animate_button = document.getElementById('animate_button');
-  var stop_button = document.getElementById('stop_button');
-  var reset_button=document.getElementById('reset_button');
-  var count_field = document.getElementById('rect_qty');
-  var rect_width = document.getElementById('rect_width');
-  var rect_height = document.getElementById('rect_height');
+  var f = document.querySelector('form');
+
+  //var count_field = document.getElementById('rect_qty');
+  //var rect_width = document.getElementById('rect_width');
+  //var rect_height = document.getElementById('rect_height');
   var x=0, y=0, width = myCanvas.width, height = myCanvas.height;
   var offset = 0;
   var interval;
+  f.n.focus();
   var grd = context.createLinearGradient(0,0,width,height);
   grd.addColorStop(0,"red");
   grd.addColorStop(.5,"orange");
@@ -16,15 +16,16 @@
   context.fillStyle = grd;
   //context.fillRect(10,10,width,height);
 
-  function drawManyRectangles(){
+  function drawManyRectangles(ev){
+    ev.preventDefault();
     // debugger;  //used debugger to see what 'this' keyword returned ... fyi it was animate_button
   	context.fillRect(0,0,width,height);
-    width = rect_width.value;
-  	height = rect_height.value;
+    width = f.w.value;
+  	height = f.h.value;
 
     var redraw = function(){
       clear();
-      for (var i=0; i < count_field.value; i++) {
+      for (var i=0; i < f.n.value; i++) {
     		x = i/10 * myCanvas.width + offset;
         y = i/10 * myCanvas.height + offset;
     		context.fillRect(x, y, width, height);
@@ -42,13 +43,15 @@
   function clear() {
       context.clearRect(0, 0, myCanvas.width, myCanvas.height);
   }
-  animate_button.onclick = drawManyRectangles;
-  stop_button.onclick = stop;
-  reset_button.onclick = function(){
+  f.onsubmit = drawManyRectangles;
+
+  f.stop.onclick = stop;
+
+  f.reset.onclick = function(){
     offset = 0;
     clear();
-    count_field.focus();
-    count_field.select();
+    f.n.focus();
+    f.n.select();
     //document.getElementById('rect_qty').value = 20;
   };
 })();
