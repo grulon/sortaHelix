c75f5ffb5892cb1d296bb9b07926fa0e4f4354a9
Gavin Rulon <gavin.rulon@icloud.com>
2015-08-16 20:41:06 -0400

    Inputs from form and pendulum swinging.

--- js/helix.js
+++ js/helix.js
@@ -7,7 +7,7 @@
   //var rect_height = document.getElementById('rect_height');
   var x=0, y=0, width = myCanvas.width, height = myCanvas.height;
   var offset = 0;
-  var interval;
+  var interval, intervals=[];
   f.n.focus();
   var grd = context.createLinearGradient(0,0,width,height);
   grd.addColorStop(0,"red");
@@ -36,6 +36,64 @@
     interval= setInterval(redraw, 20);
   }
 
+  function animateCircle(ev) {
+      ev.preventDefault();
+      // debugger;  //used debugger to see what 'this' keyword returned ... fyi it was animate_button
+      context.fillRect(0,0,width,height);
+      width = f.w.value;
+      height = f.h.value;
+
+      var redraw = function(){
+        var sin, cos;
+        clear();
+        width = f.w.value*Math.random();
+        height = f.h.value*Math.random();
+        for (var i=0; i < f.n.value; i++) {
+          //width = f.w.value*Math.random();
+          //height = f.h.value*Math.random();
+          sin = Math.sin(i / 20 + offset) / 2 + 0.5;
+          cos = Math.cos(i / 20 + offset) / 2 + 0.5;
+          x = sin * (myCanvas.width - width);
+          y = cos * (myCanvas.height - height);
+          context.fillRect(x, y, width, height);
+         }
+         offset += 0.1;
+      };
+      stop();
+      interval= setInterval(redraw, 20);
+  }
+
+  function pendulums(ev) {
+      ev.preventDefault();
+      // debugger;  //used debugger to see what 'this' keyword returned ... fyi it was animate_button
+      context.fillRect(0,0,width,height);
+      width = f.w.value;
+      height = f.h.value;
+
+      var redraw = function(){
+        clear();
+        width = f.w.value;
+        height = f.h.value;
+        for (var i=0; i < f.n.value; i++) {
+          x = myCanvas.width / 2 + Math.sin(offset * i) * f.m.value;
+          y = i / f.n.value * myCanvas.height;
+
+          context.fillRect(x, y, width, height);
+         }
+         offset += f.o.value;
+      };
+      stopPendulums();
+      intervals.push(setInterval(redraw, f.i.value));
+  }
+
+  function stopPendulums() {
+    while(intervals.length > 0) {
+      var removed_item = intervals.pop();
+      clearInterval(removed_item);
+    }
+  }
+
+
   function stop() {
     clearInterval(interval);
   }
@@ -43,9 +101,11 @@
   function clear() {
       context.clearRect(0, 0, myCanvas.width, myCanvas.height);
   }
-  f.onsubmit = drawManyRectangles;
+  //f.onsubmit = drawManyRectangles;
+  // f.onsubmit = animateCircle;
+  f.onsubmit = pendulums;
 
-  f.stop.onclick = stop;
+  f.stop.onclick = stopPendulums;
 
   f.reset.onclick = function(){
     offset = 0;
